4d390f3581af281f6f1a7052f5d81194b80ae60c
George Goering <ggoeringgg@gmail.com>
2017-03-28 17:37:00 -0600

    got file upload button to show filename

--- scripts.js
+++ scripts.js
@@ -1,20 +1,42 @@
-var titleGen = getElementById('title-gen');
-var captionGen = getElementById('caption-gen');
-var chooseFileBtn = getElementById('choose-file-btn');
-var saveBtn = getElementById('choose-file-btn');
-var cardDelBtn = getElementsByClassName('card-del-btn')
-var favIconBtn = getElementsByClassName('card-icon-fav')
+// var titleGen = getElementById('title-gen');
+// var captionGen = getElementById('caption-gen');
+// var chooseFileBtn = getElementById('choose-file-btn');
+// var saveBtn = getElementById('choose-file-btn');
+// var cardDelBtn = getElementsByClassName('card-del-btn')
+// var favIconBtn = getElementsByClassName('card-icon-fav')
+//
+// function AlbumCard {
+//   this.caption = caption;
+//   this.title = title;
+// }
+//
+// AlbumCard.prototype.image = function() {
+// }
+//
+// AlbumCard.prototype.delete = function() {
+//   this = null;
+// }
+//
+// delBtn.addEventListener('click', delete())
 
-function AlbumCard {
-  this.caption = caption;
-  this.title = title;
-}
 
-AlbumCard.prototype.image = function() {
-}
+var inputs = document.querySelectorAll( '.inputfile' );
+Array.prototype.forEach.call( inputs, function( input )
+{
+	var label	 = input.nextElementSibling,
+		labelVal = label.innerHTML;
 
-AlbumCard.prototype.delete = function() {
-  this = null;
-}
+	input.addEventListener( 'change', function( e )
+	{
+		var fileName = '';
+		if( this.files && this.files.length > 1 )
+			fileName = ( this.getAttribute( 'data-multiple-caption' ) || '' ).replace( '{count}', this.files.length );
+		else
+			fileName = e.target.value.split( '\\' ).pop();
 
-delBtn.addEventListener('click', delete())
+		if( fileName )
+			label.getElementById( 'span' ).innerHTML = fileName;
+		else
+			label.innerHTML = labelVal;
+	});
+});
