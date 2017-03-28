d14f985986da001e0f64d2228a71d078bd481ca4
George Goering <ggoeringgg@gmail.com>
2017-03-27 21:29:14 -0600

    finished button styling for file upload. Initialized some variables in javascript.

---
+++ scripts.js
@@ -0,0 +1,4 @@
+var titleGen = getElementById('title-gen');
+var captionGen = getElementById('caption-gen');
+var chooseFileBtn = getElementById('choose-file-btn');
+var saveBtn = getElementById('choose-file-btn');
